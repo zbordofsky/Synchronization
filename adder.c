d187e5538793f051728b24289d4de00ac452a6e9
Zachary Bordofsky <ZEB@s-169-232-117-72.resnet.ucla.edu>
2016-03-08 20:59:01 -0800

    progress

--- adder.c
+++ adder.c
@@ -10,6 +10,7 @@ void *threadAction(void *pointer);
 void add(long long *pointer, long long value); 
 
 long long *COUNTER = 0; 
+
 int main(int argc, char *argv[]) { 
 	//timer function 
 	testerFunc(argc, argv); 
@@ -36,29 +37,31 @@ void testerFunc (int argc, char *argv[])
 		{
 			case 'i':
 			if(optarg != NULL)
-				iterations = optarg; 
+				iterations = atoi(optarg); 
 			break; 
 
 			case 't':
 			if(optarg != NULL)
-				numThreads = optarg; 
+				numThreads = atoi(optarg); 
 			break; 
 
 		}
 		opt = getopt_long(argc, argv, "a", long_options, &option_index); 
 	}
-	pthread_t threads[numThreads]; // = malloc(sizeof(pthread_t)*numThreads*2);  
+	pthread_t *threads = malloc(sizeof(pthread_t)*numThreads);  
 	int i; 
-	int ret; 
+	int ret = 1; 
+	printf("The threads are: %d\n", numThreads); 
 	for(i = 0; i<numThreads; i++){
-		ret = pthread_create(&threads[i], NULL, threadAction, (void *) COUNTER); 
+		ret = pthread_create(&threads[i], NULL, threadAction, (void *) &COUNTER); 
 		if(ret!=0){ 
-			fprintf(stderr, "Issue creating thread"); 
+			fprintf(stderr, "Issue creating thread\n"); 
 		}
 
 	}
+	
 	for(i = 0; i < numThreads; i++) { 
-		//ret = pthread_join(threads[i], NULL); 
+		ret = pthread_join(threads[i], NULL); 
 		if(ret != 0){
 			fprintf(stderr, "Isse joining threads"); 
 		}
