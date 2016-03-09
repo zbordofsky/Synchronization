8f6be5823af03a97a291f0a5697e7cf8ebfbdfeb
Zachary Bordofsky <ZEB@s-164-67-2-55.resnet.ucla.edu>
2016-03-09 01:03:52 -0800

    almost there

--- adder.c
+++ adder.c
@@ -3,18 +3,31 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <time.h>
+#define BILLION  1000000000;
 
 void testerFunc (int argc, char *argv[]); 
 void *threadAction(void *pointer); 
+void printInfo(struct timespec begin, struct timespec end); 
 //void *minusOne(void *pointer); 
+int iterations = 1; 
+int numThreads = 1; 
+int syncMethod = 0; 
+int opt_yield = 0; 
+int test_lock = 0; 
+pthread_mutex_t mutexAdd; 
 void add(long long *pointer, long long value); 
 
 long long *COUNTER = 0; 
 
 int main(int argc, char *argv[]) { 
 	//timer function 
+	struct timespec beginning, end; 
+	clock_gettime(CLOCK_REALTIME, &beginning); 
 	testerFunc(argc, argv); 
-	//timer function 
+	clock_gettime(CLOCK_REALTIME, &end); 
+	printInfo(beginning, end); 
+	pthread_mutex_destroy(&mutexAdd); 
 }
 
 
@@ -24,11 +37,12 @@ void testerFunc (int argc, char *argv[])
 	{
 		{"iterations", optional_argument, 0, 'i'}, 
 		{"threads", optional_argument, 0, 't'}, 
+		{"yield", optional_argument, 0, 'y'},
+		{"sync", optional_argument, 0, 's'}, 
 		{0, 0, 0, 0}
 	}; 
 	int opt; 
-	int iterations = 1; 
-	int numThreads = 1; 
+	
 	int option_index = 0; 
 	opt = getopt_long(argc, argv, "a", long_options, &option_index); 
 	while(opt != -1) 
@@ -44,6 +58,22 @@ void testerFunc (int argc, char *argv[])
 			if(optarg != NULL)
 				numThreads = atoi(optarg); 
 			break; 
+			
+			case 'y': 
+				opt_yield = 1; 
+			break; 
+			
+			case 's': 
+				/*if(optarg[0] != '\0'){ 
+					if(optarg[0] = 'm') { 
+						syncMethod = 1; 
+						pthread_mutex_init(&mutexAdd, NULL); 
+					}	 
+					else if(optarg[0] = 's') { 
+						syncMethod = 2; 
+					} 
+				} */
+				break; 
 
 		}
 		opt = getopt_long(argc, argv, "a", long_options, &option_index); 
@@ -66,28 +96,66 @@ void testerFunc (int argc, char *argv[])
 			fprintf(stderr, "Isse joining threads"); 
 		}
 	}
-	//long long count = COUNTER; 
-	//printf("The count is: %d\n", count); 
-	if( COUNTER == 0){
-		printf("hey it works!\n"); 
-	}
-	else{ 
-		printf("the counter is: %d\n", COUNTER); 
-	}
 }
 
 void *threadAction(void *pointer) { 
-	add(pointer, 1); 
-	add(pointer, -1); 
+	int j; 
+	switch(syncMethod) 
+	{/*
+		case 1: 
+			for(j=0; j< iterations; j++) { 
+				pthread_mutex_lock(&mutexAdd); 
+				add(pointer, 1); 
+				add(pointer, -1); 
+				pthread_mutex_unlock(&mutexAdd); 
+			}
+		break; 
+		
+		case 2: 
+			for(j=0; j < iterations; j++) { 
+				while(__sync_lock_test_and_set(&test_lock, 1)); 
+				add(pointer, 1); 
+				add(pointer, -1); 
+				__sync_lock_release(&test_lock); 
+			} 
+			break; 
+			*/
+		case 0: 
+			for(j=0; j < iterations; j++) { 
+				add(pointer, 1); 
+				add(pointer, -1); 
+			}
+		break; 
+	}
 }
-/*
-void *minusOne(void *pointer) { 
-	add(pointer, -1); 
-}*/
 
+void printInfo(struct timespec begin, struct timespec end){ 
+	time_t secs = (end.tv_sec - begin.tv_sec) * 1000000000; 
+	long int nsecs; 
+	if( end.tv_nsec > begin.tv_nsec)
+		nsecs = end.tv_nsec - begin.tv_nsec; 
+	else
+		nsecs = begin.tv_nsec- end.tv_nsec; 
+	long int totalTime = (long int) secs + nsecs; 
+	
+	int operations = numThreads * iterations * 2; 
+	printf("hello\n"); 
+	printf("the count is: %lld\n", COUNTER); 
+	
+	printf("%d threads x %d iterations x (add + subtract) = %d operations\n", numThreads, iterations, operations); 
+	if(COUNTER == 0)
+		printf("Everything worked!\n"); 
+	else
+		printf("Error: counter = %lld\n", *COUNTER); 
+	printf("Elapsed time: %ld ns\n", totalTime); 
+	int perOp = totalTime / operations; 
+	printf("per operation: %d ns\n", perOp); 
+}
 
 void add(long long *pointer, long long value)
 { 
 	long long sum = *pointer + value; 
+	if(opt_yield)
+		pthread_yield(); 
 	*pointer = sum; 
 }
