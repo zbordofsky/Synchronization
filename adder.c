6dea48cd9e848b3a47e490f9b8e6877c98298c83
Zachary Bordofsky <ZEB@s-169-232-117-127.resnet.ucla.edu>
2016-03-09 18:17:30 -0800

    part1 done

--- adder.c
+++ adder.c
@@ -15,6 +15,7 @@ int numThreads = 1;
 int syncMethod = 0; 
 int opt_yield = 0; 
 int test_lock = 0; 
+int lock = 0; 
 pthread_mutex_t mutexAdd; 
 void add(long long *pointer, long long value); 
 
@@ -64,15 +65,18 @@ void testerFunc (int argc, char *argv[])
 			break; 
 			
 			case 's': 
-				/*if(optarg[0] != '\0'){ 
-					if(optarg[0] = 'm') { 
+				if(optarg[0] != '\0'){ 
+					if(optarg[0] == 'm') { 
 						syncMethod = 1; 
 						pthread_mutex_init(&mutexAdd, NULL); 
 					}	 
-					else if(optarg[0] = 's') { 
+					else if(optarg[0] == 's') { 
 						syncMethod = 2; 
-					} 
-				} */
+					}
+					else if(optarg[0] == 'c'){ 
+						syncMethod = 3; 
+					}
+				} 
 				break; 
 
 		}
@@ -101,7 +105,7 @@ void testerFunc (int argc, char *argv[])
 void *threadAction(void *pointer) { 
 	int j; 
 	switch(syncMethod) 
-	{/*
+	{
 		case 1: 
 			for(j=0; j< iterations; j++) { 
 				pthread_mutex_lock(&mutexAdd); 
@@ -119,9 +123,18 @@ void *threadAction(void *pointer) {
 				__sync_lock_release(&test_lock); 
 			} 
 			break; 
-			*/
+			
+		case 3: 
+			for(j = 0; j < iterations; j++) { 
+				while(__sync_val_compare_and_swap(&lock, 0, 1) == 1); 
+				add(pointer, 1); 
+				add(pointer, -1); 
+				lock = 0; 
+			}
+			break; 
 		case 0: 
 			for(j=0; j < iterations; j++) { 
+				//printf("counter is: %lld\n", COUNTER); 
 				add(pointer, 1); 
 				add(pointer, -1); 
 			}
@@ -139,14 +152,12 @@ void printInfo(struct timespec begin, struct timespec end){
 	long int totalTime = (long int) secs + nsecs; 
 	
 	int operations = numThreads * iterations * 2; 
-	printf("hello\n"); 
-	printf("the count is: %lld\n", COUNTER); 
 	
 	printf("%d threads x %d iterations x (add + subtract) = %d operations\n", numThreads, iterations, operations); 
 	if(COUNTER == 0)
 		printf("Everything worked!\n"); 
 	else
-		printf("Error: counter = %lld\n", *COUNTER); 
+		printf("Error: counter = %lld\n", COUNTER); 
 	printf("Elapsed time: %ld ns\n", totalTime); 
 	int perOp = totalTime / operations; 
 	printf("per operation: %d ns\n", perOp); 
